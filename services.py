e41fba4cfa36aa457a3ffd69921e0a2c9494ce83
David Tomicek <tomicek.david@yahoo.com>
2021-08-17 13:12:34 +0200

    making a dict with commands a symlinks

--- services.py
+++ services.py
@@ -16,10 +16,22 @@ import sys
 import time
 import subprocess
 import logging
+import os
 
 
-def init_commands(dirname, services):
-    return list(map(lambda x: f'cd {dirname} && ./service{x}/run', services))
+def init_commands(dirname):
+    # return list(map(lambda x: f'cd {dirname} && ./service{x}/run', services))
+    symlinks = os.listdir(path=f'./{dirname}')
+    ret = []
+    for symlink in symlinks:
+        fn = os.listdir(path=f'./{dirname}/{symlink}')
+        command = f'./{fn[0]}'
+        d = {
+            'path': f'./{dirname}/{symlink}',
+            'run_filename': command
+        }
+        ret.append(d)
+    return ret
 
 
 def show_outputs(command, returncode, stdout):
@@ -32,13 +44,13 @@ def show_outputs(command, returncode, stdout):
 
 def run_services(commands):
     for command in commands:
-        process = subprocess.run(command, capture_output=True, shell=True)
-        show_outputs(command, process.returncode, process.stdout.decode())
+        process = subprocess.run(command['run_filename'], capture_output=True, shell=True, cwd=command['path'])
+        show_outputs(process, process.returncode, process.stdout.decode())
         if process.returncode != 0:
             logging.info('Failed to execute: %s' % command)
             logging.info('Return code: %s' % process.returncode)
             return 1
-    # failed_services = list(filter(lambda z: z != 0, list(map(lambda y: show_outputs(y, y.returncode, y.stdout.decode()), list(map(lambda x: subprocess.run(x, capture_output=True, shell=True), commands))))))
+    # failed_services = list(filter(lambda z: z != 0, map(lambda y: show_outputs(y, y.returncode, y.stdout.decode()), map(lambda x: subprocess.run(x, capture_output=True, shell=True), commands))))
     # if failed_services:
     #     logging.info('%s failed.' % failed_services[0])
 
@@ -48,8 +60,7 @@ def main():
     logging.basicConfig(level='DEBUG', filename='services.log')
     logging.getLogger().addHandler(logging.StreamHandler()) # print logging messages to console
     dirname = args['<dirname>']
-    services = [1, 2, 3]
-    commands = init_commands(dirname, services)
+    commands = init_commands(dirname)
     while 1:
         error_check = run_services(commands)
         if error_check:
