35ae7163117ad7febba7bfb304e9c713350ee9d2
David Tomicek <tomicek.david@yahoo.com>
2021-08-24 10:29:16 +0200

    service file automatically installed to /usr/lib/systemd/system/

--- setup.py
+++ setup.py
@@ -6,9 +6,12 @@ setup(
     name='dockerservices',
     version='2.2.2',
     description='Start docker services',
-    long_description='Start docker services.',
+    long_description='Start docker services ....',
     packages=['dockerservices'],
     install_requires=['docopt'],
     package_dir={'':'src'},
     entry_points={"console_scripts": ["dockerservices = dockerservices.dockerservices:main"]},
+    include_package_data=True,
+    data_files=[("/usr/lib/systemd/system/", ["dockerservices.service"])],
+    package_data={"": ["dockerservices.service"]},
 )
