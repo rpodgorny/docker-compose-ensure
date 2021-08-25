46a5a68bc893ba9d3d3c247bb002b2560563af96
David Tomicek <tomicek.david@yahoo.com>
2021-08-25 09:39:27 +0200

    loop to main method

--- src/dockerservices/dockerservices.py
+++ src/dockerservices/dockerservices.py
@@ -24,13 +24,6 @@ import logging
 import os
 
 
-def run_services(dirs, command, shell_):
-    for dir_ in dirs:
-        logging.info('Command executed: %s', command)
-        process = subprocess.run(command, capture_output=True, shell=shell_, cwd=dir_)
-        logging.info('Return code: %s', process.returncode)
-
-
 def main():
     args = docopt.docopt(__doc__)
     logging.basicConfig(level='DEBUG')
@@ -38,14 +31,17 @@ def main():
     pure_command = args['<command>']
     if '--' in pure_command:
         pure_command.pop(0)
-    shell = args['--shell']
-    command = ' '.join(pure_command) if shell else pure_command
+    shell_ = args['--shell']
+    command = ' '.join(pure_command) if shell_ else pure_command
     sleep_time = args['--sleep']
     sleep_time = float(sleep_time) if sleep_time else 5
     dirs = [f'./{dirname}/{x}' for x in os.listdir(dirname) if os.path.islink(f'./{dirname}/{x}')]
     while 1:
-        run_services(dirs, command, shell)
-        time.sleep(sleep_time)
+        for dir_ in dirs:
+          logging.info('Command executed: %s', command)
+          process = subprocess.run(command, capture_output=True, shell=shell_, cwd=dir_)
+          logging.info('Return code: %s', process.returncode)
+          time.sleep(sleep_time)
 
 
 if __name__ == "__main__":
