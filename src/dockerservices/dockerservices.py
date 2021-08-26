291c9c319e863e6236fb2d8a1ad072ca4d8f94d2
David Tomicek <tomicek.david@yahoo.com>
2021-08-26 16:01:48 +0200

    formatting

--- src/dockerservices/dockerservices.py
+++ src/dockerservices/dockerservices.py
@@ -37,7 +37,6 @@ def main():
     sleep_time = float(sleep_time) if sleep_time else 5
     dirs = [f'./{dirname}/{x}' for x in os.listdir(dirname) if os.path.islink(f'./{dirname}/{x}')]
     times = [sleep_time for x in range(len(dirs))]
-    print(times, ' times')
     while 1:
         for dir_ in dirs:
           index = dirs.index(dir_)
