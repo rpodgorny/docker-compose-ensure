6941fe444d5776e0ecc352760737f8690c015d4f
David Tomicek <tomicek.david@yahoo.com>
2021-08-26 15:43:20 +0200

    increasing sleep time if return code not 0

--- src/dockerservices/dockerservices.py
+++ src/dockerservices/dockerservices.py
@@ -36,12 +36,21 @@ def main():
     sleep_time = args['--sleep']
     sleep_time = float(sleep_time) if sleep_time else 5
     dirs = [f'./{dirname}/{x}' for x in os.listdir(dirname) if os.path.islink(f'./{dirname}/{x}')]
+    times = []
+    for i in range(len(dirs)):
+          times.append(sleep_time)
+    print('times: ', times)
     while 1:
         for dir_ in dirs:
+          index = dirs.index(dir_)
+          time_index = times[index]
           logging.info('Command executed: %s', command)
           process = subprocess.run(command, capture_output=True, shell=shell_, cwd=dir_)
+          if process.returncode != 0:
+            time_index += time_index
+          times[index] = time_index
           logging.info('Return code: %s', process.returncode)
-          time.sleep(sleep_time)
+          time.sleep(time_index)
 
 
 if __name__ == "__main__":
