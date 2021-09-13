72de843b33e18b90a27ad7ce25c95c6cf784dd33
Radek Podgorny <radek@podgorny.cz>
2021-09-13 12:54:13 +0200

    fixes and fixme updates

--- src/dockerservices/dockerservices.py
+++ src/dockerservices/dockerservices.py
@@ -11,8 +11,8 @@ Arguments:
   <command>  Command that will be executed.
 
 Options:
-  --check-delay <check-delay>  Specify interval how often process will be checked (secs).
-  --shell                      Set shell arg to True.
+  --check-delay=<n>  Specify interval how often process will be checked (secs).
+  --shell            Set shell arg to True.
 '''
 
 
@@ -29,6 +29,7 @@ def main():
     logging.basicConfig(level='DEBUG')
     dirname = args['<dirname>']
     pure_command = args['<command>']
+    # FIXME: fun task - rewrite the following using ternary operator ;-)
     if '--' in pure_command:
         pure_command.pop(0)
     shell_ = args['--shell']
@@ -43,20 +44,20 @@ def main():
                     'interval': check_delay,
                     't_last': 0,
                 }}
-            d.update(items_creation)
-        list(filter(lambda x: d.pop(x) if x in list(d.keys()) and x not in check_dirs else None, list(d.keys())))
+            d.update(items_creation)  # FIXME: is this indented correctly?
+        list(filter(lambda x: d.pop(x) if x in list(d.keys()) and x not in check_dirs else None, list(d.keys())))  # FIXME: this is abusing of functional constructs - either go fully functional (reduce?) or rewrite to plain old simple for loop ;-)
 
         t = time.time()
         for k, v in d.items():
             if t - v['interval'] > v['t_last']:
-                logging.info('Command executed: %s', command)
+                logging.info('Will execute: %s', command)
                 process = subprocess.run(command, shell=shell_, cwd=k)
-                v['interval'] = min(v['interval'] * 2, 800) if process.returncode != 0 else check_delay
+                v['interval'] = min(v['interval'] * 2, 800) if process.returncode != 0 else check_delay  # FIXME: make 800 a global constant
                 v['t_last'] = t
                 logging.info('Return code: %s', process.returncode)
             else:
-                logging.info('Not running, waiting for delay.')
-        time.sleep(10)
+                logging.info('Not running, waiting for delay.')  # FIXME: remove this - it generates too much noise
+        time.sleep(10)  # FIXME: make this a global constant
     return 0
 
 
